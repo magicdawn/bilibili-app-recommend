d3be5831e06aa118e3e2772713a2a3ce52e5ef16
magicdawn <magicdawn@qq.com>
2022-05-28 22:22:44 +0800

    chore: tweak

--- scripts/install.ts
+++ scripts/install.ts
@@ -9,7 +9,8 @@ const url = `file://${projectRoot}/dist/${name}.user.js`
 
 console.log('-'.repeat(50))
 console.log(`script url:`)
-console.log(`serve: ${chalk.yellow('http://127.0.0.1:3000/__vite-plugin-monkey.install.user.js')}`)
 console.log(`build: ${chalk.green(url)}`)
 console.log('open this url in Chrome with ViolentMonkey installed')
 console.log('-'.repeat(50))
+
+// console.log(`serve: ${chalk.yellow('http://127.0.0.1:3000/__vite-plugin-monkey.install.user.js')}`)
