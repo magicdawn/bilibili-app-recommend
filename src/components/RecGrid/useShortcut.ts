04cb27434e3f810aa2e1b60f7da07072eda15f7d
Tao <magicdawn@qq.com>
2023-06-21 02:11:07 +0800

    fix: use different cache in getColumnCount, since new feature may change column count

--- src/components/RecGrid/useShortcut.ts
+++ src/components/RecGrid/useShortcut.ts
@@ -206,13 +206,15 @@ export function useShortcut({
 }
 
 // use window.innerHeight as cache key
-const countCache = new Map<number, number>()
+const countCache1 = new Map<number, number>()
+const countCache2 = new Map<number, number>()
 
 // SectionRecommend 没有 narrow-mode
 // RecGrid 有 narrow mode
 export function getColumnCount(container?: HTMLElement | null, mayHaveNarrowMode = true) {
   if (mayHaveNarrowMode && settings.useNarrowMode) return 2
 
+  const countCache = settings.styleUseYoutubeLikeCard ? countCache1 : countCache2
   let count = countCache.get(Math.trunc(window.innerWidth))
   if (count) {
     return count
