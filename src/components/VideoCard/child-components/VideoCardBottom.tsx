f53651e11df3676a8c7a789c8f70147dd9aa2154
Tao <magicdawn@qq.com>
2024-04-25 18:36:38 +0800

    feat: use react-if

--- src/components/VideoCard/child-components/VideoCardBottom.tsx
+++ src/components/VideoCard/child-components/VideoCardBottom.tsx
@@ -2,6 +2,7 @@ import { useSettingsSnapshot } from '$modules/settings'
 import { getAvatarSrc } from '$utility/image'
 import { Avatar } from 'antd'
 import type { MouseEventHandler } from 'react'
+import { Else, If, Then } from 'react-if'
 import styles from '../index.module.scss'
 import type { IVideoCardData } from '../process/normalize'
 import { DESC_SEPARATOR } from '../process/normalize'
@@ -68,134 +69,141 @@ export function VideoCardBottom({
 
   return (
     <>
-      {/* old, same as bilibili default */}
-      {!styleNewCardStyle && (
-        <div className='bili-video-card__info __scale-disable'>
-          <div className='bili-video-card__info--right'>
-            <a
-              href={href}
-              target='_blank'
-              data-mod='partition_recommend'
-              data-idx='content'
-              data-ext='click'
-              onClick={handleVideoLinkClick}
-            >
-              <h3 className='bili-video-card__info--tit' title={title}>
-                {titleRender ?? title}
-              </h3>
-            </a>
-            <p className='bili-video-card__info--bottom'>
-              {isNormalVideo ? (
-                <a
-                  className='bili-video-card__info--owner'
-                  href={authorHref}
-                  target='_blank'
-                  title={descTitle}
-                >
-                  {recommendReason ? (
-                    <span className={styles.recommendReason}>{recommendReason}</span>
-                  ) : (
-                    <svg className='bili-video-card__info--owner__up'>
-                      <use href='#widget-up'></use>
-                    </svg>
-                  )}
-                  {descInnerSpans}
-                </a>
-              ) : appBadge || appBadgeDesc ? (
-                <a className='bili-video-card__info--owner' href={href} target='_blank'>
-                  <span className={styles.badge}>{appBadge || ''}</span>
-                  <span className={styles.bangumiDesc}>{appBadgeDesc || ''}</span>
-                </a>
-              ) : null}
-            </p>
-          </div>
-        </div>
-      )}
-
-      {/* new, not so crowded */}
-      {styleNewCardStyle && (
-        <div
-          css={css`
-            display: flex;
-            margin-top: 15px;
-          `}
-        >
-          <a href={authorHref} target='_blank' onClick={handleVideoLinkClick}>
-            {authorFace ? (
-              <Avatar src={getAvatarSrc(authorFace)} />
-            ) : (
-              <Avatar>{authorName?.[0] || appBadgeDesc?.[0] || ''}</Avatar>
-            )}
-          </a>
-
-          <div
-            css={css`
-              flex: 1;
-              margin-left: 10px;
-              overflow: hidden;
-            `}
-          >
-            <h3
-              className='bili-video-card__info--tit'
-              title={title}
-              css={css`
-                .bili-video-card &.bili-video-card__info--tit {
-                  padding-right: 0;
-                  height: auto;
-                  max-height: calc(2 * var(--title-line-height));
-                }
-              `}
-            >
+      <If condition={styleNewCardStyle}>
+        {/* old, same as bilibili default */}
+        <Else>
+          <div className='bili-video-card__info __scale-disable'>
+            <div className='bili-video-card__info--right'>
               <a
                 href={href}
                 target='_blank'
-                rel='noopener'
-                css={css`
-                  .bili-video-card .bili-video-card__info--tit > a& {
-                    font-family: inherit;
-                    font-weight: initial;
-                  }
-                `}
+                data-mod='partition_recommend'
+                data-idx='content'
+                data-ext='click'
+                onClick={handleVideoLinkClick}
               >
-                {titleRender ?? title}
+                <h3 className='bili-video-card__info--tit' title={title}>
+                  {titleRender ?? title}
+                </h3>
               </a>
-            </h3>
-
-            {isNormalVideo ? (
-              <>
-                <div>
+              <p className='bili-video-card__info--bottom'>
+                {isNormalVideo ? (
                   <a
                     className='bili-video-card__info--owner'
                     href={authorHref}
                     target='_blank'
                     title={descTitle}
-                    css={descCss}
                   >
+                    {recommendReason ? (
+                      <span className={styles.recommendReason}>{recommendReason}</span>
+                    ) : (
+                      <svg className='bili-video-card__info--owner__up'>
+                        <use href='#widget-up'></use>
+                      </svg>
+                    )}
                     {descInnerSpans}
                   </a>
-                </div>
-                {recommendReason && (
-                  <div
-                    className={styles.recommendReason}
-                    css={css`
-                      margin-top: 2px;
-                      padding-left: 0;
-                      max-width: 100%;
-                    `}
-                  >
-                    {recommendReason}
-                  </div>
-                )}
-              </>
-            ) : appBadge || appBadgeDesc ? (
-              <a className='bili-video-card__info--owner' css={descCss} href={href} target='_blank'>
-                {!!appBadge && <span className={styles.badge}>{appBadge}</span>}
-                {!!appBadgeDesc && <span className={styles.bangumiDesc}>{appBadgeDesc}</span>}
-              </a>
-            ) : null}
+                ) : appBadge || appBadgeDesc ? (
+                  <a className='bili-video-card__info--owner' href={href} target='_blank'>
+                    <span className={styles.badge}>{appBadge || ''}</span>
+                    <span className={styles.bangumiDesc}>{appBadgeDesc || ''}</span>
+                  </a>
+                ) : null}
+              </p>
+            </div>
           </div>
-        </div>
-      )}
+        </Else>
+
+        {/* new, not so crowded */}
+        <Then>
+          <div
+            css={css`
+              display: flex;
+              margin-top: 15px;
+            `}
+          >
+            <a href={authorHref} target='_blank' onClick={handleVideoLinkClick}>
+              {authorFace ? (
+                <Avatar src={getAvatarSrc(authorFace)} />
+              ) : (
+                <Avatar>{authorName?.[0] || appBadgeDesc?.[0] || ''}</Avatar>
+              )}
+            </a>
+
+            <div
+              css={css`
+                flex: 1;
+                margin-left: 10px;
+                overflow: hidden;
+              `}
+            >
+              <h3
+                className='bili-video-card__info--tit'
+                title={title}
+                css={css`
+                  .bili-video-card &.bili-video-card__info--tit {
+                    padding-right: 0;
+                    height: auto;
+                    max-height: calc(2 * var(--title-line-height));
+                  }
+                `}
+              >
+                <a
+                  href={href}
+                  target='_blank'
+                  rel='noopener'
+                  css={css`
+                    .bili-video-card .bili-video-card__info--tit > a& {
+                      font-family: inherit;
+                      font-weight: initial;
+                    }
+                  `}
+                >
+                  {titleRender ?? title}
+                </a>
+              </h3>
+
+              {isNormalVideo ? (
+                <>
+                  <div>
+                    <a
+                      className='bili-video-card__info--owner'
+                      href={authorHref}
+                      target='_blank'
+                      title={descTitle}
+                      css={descCss}
+                    >
+                      {descInnerSpans}
+                    </a>
+                  </div>
+                  {recommendReason && (
+                    <div
+                      className={styles.recommendReason}
+                      css={css`
+                        margin-top: 2px;
+                        padding-left: 0;
+                        max-width: 100%;
+                      `}
+                    >
+                      {recommendReason}
+                    </div>
+                  )}
+                </>
+              ) : appBadge || appBadgeDesc ? (
+                <a
+                  className='bili-video-card__info--owner'
+                  css={descCss}
+                  href={href}
+                  target='_blank'
+                >
+                  {!!appBadge && <span className={styles.badge}>{appBadge}</span>}
+                  {!!appBadgeDesc && <span className={styles.bangumiDesc}>{appBadgeDesc}</span>}
+                </a>
+              ) : null}
+            </div>
+          </div>
+        </Then>
+      </If>
     </>
   )
 }
