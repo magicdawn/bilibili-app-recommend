d4bbbe8a7c86b639bb80426a283633cb9d228bc3
magicdawn <magicdawn@qq.com>
2024-06-19 01:58:09 +0800

    chore: add comment for why use commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,12 @@ import defaultConfig from '@commitlint/config-conventional'
 import type { UserConfig } from '@commitlint/types'
 import { RuleConfigSeverity } from '@commitlint/types'
 
+/**
+ * why: 我把 performance perf, 打成了 pref, preference, 直到 release changelog 里没有, 才发现 typo
+ * 还是加个 Lint 吧
+ * see https://github.com/magicdawn/bilibili-app-recommend/commit/7439247
+ */
+
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
