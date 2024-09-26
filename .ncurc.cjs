22126a53812cd52ea6ddc46cbe4513f09d578ef2
magicdawn <magicdawn@qq.com>
2024-09-26 22:11:38 +0800

    fix(antd): revert antd to fix Button style

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  reject: ['axios'],
+  reject: [
+    'axios',
+
+    // https://ant.design/changelog-cn#5211 external antd 后, default Button 没有边框,
+    // 缺少 .ant-btn-variant-outlined.ant-btn-color-default { border-color: #d9d9d9; } 样式
+    // 仅 external 情况下会有, 无相关 issue
+    'antd',
+  ],
 }
