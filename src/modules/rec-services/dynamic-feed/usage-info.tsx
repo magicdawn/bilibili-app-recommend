b93646ece7c1e524a70790b41aa6b64488cdb670
magicdawn <magicdawn@qq.com>
2024-10-23 15:58:58 +0800

    feat(dynamic-feed): add help info for filter UI

--- src/modules/rec-services/dynamic-feed/usage-info.tsx
+++ src/modules/rec-services/dynamic-feed/usage-info.tsx
@@ -1,9 +1,10 @@
-import { antdCustomCss, iconOnlyRoundButtonCss } from '$common/emotion-css'
+import { antdCustomCss, flexVerticalCenterStyle, iconOnlyRoundButtonCss } from '$common/emotion-css'
 import { colorPrimaryValue } from '$components/ModalSettings/theme.shared'
 import { useOnRefreshContext } from '$components/RecGrid/useRefresh'
 import { CHARGE_ONLY_TEXT } from '$components/VideoCard/top-marks'
 import { IconPark } from '$modules/icon/icon-park'
 import { useSettingsSnapshot } from '$modules/settings'
+import { HelpInfo } from '$ui-components/HelpInfo'
 import { AntdTooltip } from '$ui-components/antd-custom'
 import { getAvatarSrc } from '$utility/image'
 import type { AntdMenuItemType } from '$utility/type'
@@ -38,6 +39,10 @@ const clearPayload: Partial<DynamicFeedStore> = {
 }
 
 const S = {
+  filterWrapper: css`
+    padding-block: 10px;
+  `,
+
   filterSection: css`
     width: 300px;
     margin-top: 10px;
@@ -46,9 +51,9 @@ const S = {
     }
 
     .title {
-      padding: 5px 0;
-      padding-left: 6px;
+      padding-left: 3px;
       font-size: 20px;
+      ${flexVerticalCenterStyle}
     }
     .content {
       /* margin-top: 5px; */
@@ -78,13 +83,9 @@ export function DynamicFeedUsageInfo() {
   const showFilterBadge = useMemo(() => {
     return (
       showFilter &&
-      !!(
-        dynamicFeedVideoType !== DynamicFeedVideoType.All ||
-        hideChargeOnlyVideos ||
-        (hasSelectedUp && searchText)
-      )
+      !!(dynamicFeedVideoType !== DynamicFeedVideoType.All || hideChargeOnlyVideos || searchText)
     )
-  }, [showFilter, dynamicFeedVideoType, hideChargeOnlyVideos, hasSelectedUp, searchText])
+  }, [showFilter, dynamicFeedVideoType, hideChargeOnlyVideos, searchText])
 
   // try update on mount
   useMount(() => {
@@ -188,9 +189,15 @@ export function DynamicFeedUsageInfo() {
   )
 
   const filterPopoverContent = (
-    <>
+    <div css={S.filterWrapper}>
       <div className='section' css={S.filterSection}>
-        <div className='title'>视频类型</div>
+        <div className='title'>
+          视频类型
+          <HelpInfo>
+            「{CHARGE_ONLY_TEXT}」在此程序中归类为「投稿视频」
+            <br />
+          </HelpInfo>
+        </div>
         <div className='content'>
           <Radio.Group
             buttonStyle='solid'
@@ -212,46 +219,57 @@ export function DynamicFeedUsageInfo() {
         </div>
       </div>
 
-      <div className='section' css={S.filterSection}>
-        <div className='title'>充电专属</div>
-        <div className='content'>
-          <Checkbox
-            checked={hideChargeOnlyVideos}
-            onChange={async (e) => {
-              const val = e.target.checked
-              const set = dfStore.hideChargeOnlyVideosForKeysSet
-              if (val) {
-                set.add(selectedKey)
-              } else {
-                set.delete(selectedKey)
-              }
-              await delay(100)
-              onRefresh?.()
-            }}
-            css={css`
-              margin-left: 5px;
-            `}
-          >
-            <AntdTooltip title={`隐藏「${CHARGE_ONLY_TEXT}」视频`}>
-              <span style={{ userSelect: 'none' }}>隐藏「{CHARGE_ONLY_TEXT}」</span>
-            </AntdTooltip>
-          </Checkbox>
+      {dynamicFeedVideoType !== DynamicFeedVideoType.DynamicOnly && (
+        <div className='section' css={S.filterSection}>
+          <div className='title'>充电专属</div>
+          <div className='content' css={flexVerticalCenterStyle}>
+            <Checkbox
+              checked={hideChargeOnlyVideos}
+              onChange={async (e) => {
+                const val = e.target.checked
+                const set = dfStore.hideChargeOnlyVideosForKeysSet
+                if (val) {
+                  set.add(selectedKey)
+                } else {
+                  set.delete(selectedKey)
+                }
+                await delay(100)
+                onRefresh?.()
+              }}
+              css={css`
+                margin-left: 5px;
+              `}
+            >
+              <AntdTooltip
+                title={
+                  <>
+                    隐藏「{CHARGE_ONLY_TEXT}」视频
+                    <br />
+                    程序会针对 UP 或 分组记忆你的选择~
+                  </>
+                }
+              >
+                <span style={{ userSelect: 'none' }}>隐藏「{CHARGE_ONLY_TEXT}」</span>
+              </AntdTooltip>
+            </Checkbox>
+          </div>
         </div>
-      </div>
+      )}
 
       <div className='section' css={S.filterSection}>
         <div className='title'>搜索</div>
         <div className='content'>
           <Input.Search
             style={{ width: 280 }}
-            placeholder='按标题过滤'
+            placeholder='按标题关键字过滤'
             type='search'
             autoCorrect='off'
             autoCapitalize='off'
             name={`searchText_${upMid}`}
             // 有自带的历史记录, 何乐而不为
-            // autoComplete='off'
-            autoComplete='on'
+            // 悬浮 autocomplete 时 popover 关闭了
+            // autoComplete='on'
+            autoComplete='off'
             allowClear
             onSearch={async (val) => {
               dfStore.searchText = val || undefined
@@ -261,7 +279,7 @@ export function DynamicFeedUsageInfo() {
           />
         </div>
       </div>
-    </>
+    </div>
   )
 
   return (
