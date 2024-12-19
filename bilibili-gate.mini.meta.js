fc7fa592fe20b71a16263cec9485dc99688012b5
magicdawn <magicdawn@users.noreply.github.com>
2024-12-19 18:58:12 +0000

    deploy: 9c844c72e9a810aa4be3b8403803f89c7d4cd172

---
+++ bilibili-gate.mini.meta.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Bilibili-Gate
+// @namespace    https://magicdawn.fun
+// @version      0.30.1.6-g9c844c7
+// @author       magicdawn
+// @description  Bilibili 自定义首页
+// @license      MIT
+// @icon         https://www.bilibili.com/favicon.ico
+// @homepageURL  https://greasyfork.org/zh-CN/scripts/443530-bilibili-gate
+// @supportURL   https://github.com/magicdawn/bilibili-gate/issues
+// @downloadURL  https://github.com/magicdawn/bilibili-gate/raw/release-nightly/bilibili-gate.mini.user.js
+// @updateURL    https://github.com/magicdawn/bilibili-gate/raw/release-nightly/bilibili-gate.mini.meta.js
+// @match        https://www.bilibili.com/
+// @match        https://www.bilibili.com/?*
+// @match        https://www.bilibili.com/index.html
+// @match        https://www.bilibili.com/index.html?*
+// @match        https://www.bilibili.com/video/*
+// @match        https://www.bilibili.com/list/watchlater?*
+// @match        https://www.bilibili.com/bangumi/play/*
+// @match        https://space.bilibili.com/*
+// @require      https://registry.npmmirror.com/axios/0.27.2/files/dist/axios.min.js
+// @require      https://registry.npmmirror.com/react/18.3.1/files/umd/react.production.min.js
+// @require      https://registry.npmmirror.com/react-dom/18.3.1/files/umd/react-dom.production.min.js
+// @require      https://registry.npmmirror.com/ua-parser-js/1.0.39/files/dist/ua-parser.min.js
+// @require      https://registry.npmmirror.com/framer-motion/11.15.0/files/dist/framer-motion.js
+// @require      https://registry.npmmirror.com/localforage/1.10.0/files/dist/localforage.min.js
+// @connect      app.bilibili.com
+// @grant        GM.deleteValue
+// @grant        GM.getValue
+// @grant        GM.listValues
+// @grant        GM.openInTab
+// @grant        GM.registerMenuCommand
+// @grant        GM.setClipboard
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
+// @grant        GM_addStyle
+// @grant        GM_addValueChangeListener
+// @grant        GM_download
+// @grant        unsafeWindow
+// @grant        window.focus
+// @run-at       document-end
+// ==/UserScript==
